0989a85d84abeb5263a5797e45b470cdb83e7e2f
James Armes <jamesleearmes@gmail.com>
2026-01-25 18:09:33 -0700

    Revert text height

--- client/src/font_cache.rs
+++ client/src/font_cache.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 use crate::gfx_cache::GraphicsCache;
 
 pub const BITMAP_GLYPH_W: f32 = 6.0;
-pub const BITMAP_GLYPH_H: f32 = 9.0;
+pub const BITMAP_GLYPH_H: f32 = 10.0;
 pub const BITMAP_GLYPH_COUNT: usize = 96; // ASCII 32..=127 inclusive
 pub const BITMAP_GLYPH_Y_OFFSET: f32 = 1.0; // dd.c uses +576 (one row) before glyph pixels
 pub const BITMAP_FONT_FIRST_SPRITE_ID: usize = 700;
