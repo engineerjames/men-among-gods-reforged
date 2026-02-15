c6e11e68951756666f9c05fd823c952f75545ba1
James Armes <jamesleearmes@gmail.com>
2026-02-15 14:23:08 -0700

    Roll-back log changes

--- server/src/server.rs
+++ server/src/server.rs
@@ -322,7 +322,7 @@ impl Server {
                     // TODO: Update this to be a proper moving average of the load
                     // globs.load_avg = self.tick_perf_stats.stats().mean as i32;
 
-                    log::info!(
+                    log::debug!(
                         "Tick time: {:.2} ms (max: {:.2} ms), Load: {:.2}%",
                         tick_duration,
                         self.tick_perf_stats.stats().max,
@@ -347,7 +347,7 @@ impl Server {
             let io_duration = Instant::now().duration_since(pre_io_time).as_secs_f32() * 1000.0;
             self.net_io_perf_stats.push(io_duration);
 
-            log::info!(
+            log::debug!(
                 "Network I/O time: {:.2} ms (max: {:.2} ms)",
                 io_duration,
                 self.net_io_perf_stats.stats().max,
