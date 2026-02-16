b300cf55ad6fbfe9864219e53586c0685685ff15
James Armes <jamesleearmes@gmail.com>
2026-02-15 22:00:28 -0700

    Make linking work

--- sdl_starter/src/main.rs
+++ sdl_starter/src/main.rs
@@ -1,11 +1,14 @@
 use std::time::Duration;
 
 use sdl2::event::Event;
+use sdl2::image::InitFlag;
+use sdl2::image::LoadTexture;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
+    let _image_context = sdl2::image::init(InitFlag::PNG)?;
     let video = sdl_context.video()?;
 
     let window = video
@@ -23,6 +26,10 @@ fn main() -> Result<(), String> {
         .map_err(|e| e.to_string())?;
 
     let mut event_pump = sdl_context.event_pump()?;
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture(
+        "/Users/jarmes/git/men-among-gods-reforged/client/assets/gfx/images/00001.png",
+    )?;
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -38,6 +45,9 @@ fn main() -> Result<(), String> {
 
         canvas.set_draw_color(Color::RGB(20, 20, 28));
         canvas.clear();
+
+        canvas.copy(&texture, None, None)?;
+
         canvas.present();
 
         std::thread::sleep(Duration::from_millis(16));
