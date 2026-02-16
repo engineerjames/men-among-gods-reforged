630749dbcc9f2ed11873b5350079d7f116bb7db4
James Armes <jamesleearmes@gmail.com>
2026-02-15 22:24:27 -0700

    Add basic egui test

--- sdl_starter/src/main.rs
+++ sdl_starter/src/main.rs
@@ -1,10 +1,9 @@
 use std::time::Duration;
 
+use egui_sdl2::egui::{self, Pos2};
 use sdl2::event::Event;
 use sdl2::image::InitFlag;
-use sdl2::image::LoadTexture;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -18,37 +17,95 @@ fn main() -> Result<(), String> {
         .build()
         .map_err(|e| e.to_string())?;
 
-    let mut canvas = window
-        .into_canvas()
-        .accelerated()
-        .present_vsync()
-        .build()
-        .map_err(|e| e.to_string())?;
-
     let mut event_pump = sdl_context.event_pump()?;
-    let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture(
-        "/Users/jarmes/git/men-among-gods-reforged/client/assets/gfx/images/00001.png",
-    )?;
+
+    let mut egui = egui_sdl2::EguiCanvas::new(window);
 
     'running: loop {
+        // Poll events once, handle quit and forward to egui
         for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
+            if let Event::Quit { .. }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } = event
+            {
+                break 'running;
             }
+            egui.on_event(&event);
         }
+        // Clear the egui-backed canvas
+        egui.clear([20, 20, 28, 255]);
+        // Call `run` + `paint` each frame:
+        egui.run(|ctx: &egui::Context| {
+            egui::Window::new("Account Login")
+                .default_height(800.0)
+                .default_width(600.0)
+                .fixed_pos(Pos2::new(0.0, 0.0))
+                .collapsible(false)
+                .resizable(false)
+                .show(ctx, |ui| {
+                    ui.heading("Men Among Gods Reforged");
+                    ui.add_space(10.0);
 
-        canvas.set_draw_color(Color::RGB(20, 20, 28));
-        canvas.clear();
+                    ui.label("Game server IP address");
+                    let ip_resp = ui.add_enabled(
+                        true, // TODO: FIX THIS
+                        egui::TextEdit::singleline(&mut "127.0.0.1").desired_width(260.0),
+                    );
 
-        canvas.copy(&texture, None, None)?;
+                    ui.add_space(10.0);
 
-        canvas.present();
+                    ui.label("Username");
+                    ui.add_enabled(
+                        true, // TODO: FIX THIS
+                        egui::TextEdit::singleline(&mut "").desired_width(260.0),
+                    );
+
+                    ui.add_space(8.0);
+                    ui.label("Password");
+                    ui.add_enabled(
+                        true, // TODO: FIX THIS
+                        egui::TextEdit::singleline(&mut "")
+                            .password(true)
+                            .desired_width(260.0),
+                    );
+
+                    ui.add_space(12.0);
+
+                    let (login_clicked, create_clicked) = ui
+                        .horizontal(|ui| {
+                            let login_clicked = ui
+                                .add_enabled(
+                                    true, // TODO: FIX THIS
+                                    egui::Button::new("Login").min_size([180.0, 32.0].into()),
+                                )
+                                .clicked();
+
+                            let create_clicked = ui
+                                .add_enabled(
+                                    true, // TODO: FIX THIS
+                                    egui::Button::new("Create new account")
+                                        .min_size([180.0, 32.0].into()),
+                                )
+                                .clicked();
+
+                            (login_clicked, create_clicked)
+                        })
+                        .inner;
+
+                    if login_clicked {
+                        println!("Login clicked");
+                    }
+
+                    if create_clicked {
+                        println!("Create new account clicked");
+                    }
+                });
+        });
+
+        egui.paint();
+        egui.present();
 
         std::thread::sleep(Duration::from_millis(16));
     }
