485c9a4a6c2fc528f36065b5835057a638eeea7f
James Armes <jamesleearmes@gmail.com>
2026-02-17 21:59:28 -0700

    Add two new scenes; and a new attempt at fixing linking

--- sdl_client/src/scenes/scene.rs
+++ sdl_client/src/scenes/scene.rs
@@ -15,6 +15,8 @@ pub trait Scene {
 #[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
 pub enum SceneType {
     Login,
+    //CharacterCreation,
+    //CharacterSelection,
     Game,
     NewAccount,
     Exit,
