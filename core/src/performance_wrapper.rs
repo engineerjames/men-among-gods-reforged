f4b7df7d418da2daf18d99649b8472ab9122d121
James Armes <jamesleearmes@gmail.com>
2026-07-05 20:32:38 -0600

    Add explanatory comment to measure_time

--- core/src/performance_wrapper.rs
+++ core/src/performance_wrapper.rs
@@ -1,6 +1,8 @@
 /// Wraps a function transparently unless the measure-time feature
 /// flag is utilized -- then it logs the execution time of each function
-/// wrapped.
+/// wrapped. This is meant to be used in cases where you either want to collect
+/// a lot of performance related data or not based on a compilation feature, not
+/// as a runtime-checked methodology like the client has.
 ///
 /// Returns whatever the function that is wrapped does
 #[macro_export]
