ef6cc100f645ea24beccd21938c71ac2565eb4bc
James Armes <jamesleearmes@gmail.com>
2026-08-16 20:11:44 -0600

    Fix clippy errors

--- core/src/marble.rs
+++ core/src/marble.rs
@@ -1,25 +1,26 @@
-/// "Marble bag" which provides a better player experience
-/// for situations when random chance is involved vs. a
-/// standard random number check.
-///
-/// The marble bag provides a way to guarantee that the event
-/// will occur at the appropriate frequency within a fixed number
-/// of draws.
-///
-/// For example if we wanted something to happen 25% of
-/// the time, we might generate a random float between 0 - 1
-/// and program that the event occurs when the value is <= 0.25.
-/// Here is the actual outcome of that over 10 samples that I ran
-/// when writing this structure (X is happens, O is does not happen):
-/// X O X X O X X O O O
-/// In this case, the event occurred 5 out of 10 times, or 50%, despite
-/// having a 25% probability on each individual roll. This is a perfectly
-/// valid outcome with standard random chance, but it may not produce
-/// the player experience we want.
-
-/// The marble bag instead allows us to distribute outcomes more evenly,
-/// ensuring that a 25% event occurs once within every 4 draws while still
-/// randomizing which draw it occurs on.
+//! "Marble bag" which provides a better player experience
+//! for situations when random chance is involved vs. a
+//! standard random number check.
+//!
+//! The marble bag provides a way to guarantee that the event
+//! will occur at the appropriate frequency within a fixed number
+//! of draws.
+//!
+//! For example if we wanted something to happen 25% of
+//! the time, we might generate a random float between 0 - 1
+//! and program that the event occurs when the value is <= 0.25.
+//! Here is the actual outcome of that over 10 samples that I ran
+//! when writing this structure (X is happens, O is does not happen):
+//! X O X X O X X O O O
+//! In this case, the event occurred 5 out of 10 times, or 50%, despite
+//! having a 25% probability on each individual roll. This is a perfectly
+//! valid outcome with standard random chance, but it may not produce
+//! the player experience we want.
+//!
+//! The marble bag instead allows us to distribute outcomes more evenly,
+//! ensuring that a 25% event occurs once within every 4 draws while still
+//! randomizing which draw it occurs on.
+//!
 use rand::seq::SliceRandom;
 
 const fn gcd(mut a: u32, mut b: u32) -> u32 {
