19d199b33c149d0ac133d06a5208ee86d1e25b5d
James Armes <jamesleearmes@gmail.com>
2026-09-14 22:18:39 -0600

    Correct log level

--- server/src/player/connection.rs
+++ server/src/player/connection.rs
@@ -482,7 +482,7 @@ pub fn plr_logout(gs: &mut GameState, character_id: usize, player_id: usize, rea
 
     if valid_character && reason != LogoutReason::Shutdown {
         let character_name = gs.characters[character_id].get_name().to_owned();
-        log::debug!(
+        log::info!(
             "Logging out character '{}' for reason: {:?}",
             character_name,
             reason
